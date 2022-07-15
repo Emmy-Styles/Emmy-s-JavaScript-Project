dc7cb535bc7dfdddc5a6ed1d45f47ef551f814f0
Ulelu Emmanuel <106095514+Emmy-Styles@users.noreply.github.com>
2022-07-15 20:26:27 +0000

    complete challenge

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+  deps = [
+    pkgs.mastodon
+pkgs.1.0
+pkgs.hadoop_3_0
+pkgs.1.22.19
+pkgs.v1.22.19
+pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+}
--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -46,10 +50,18 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+function celsiusToFahrenheit(celsius) {
+    const evenArray = [];
+    let truncated;
+
+    for (let i = 0; i < celsius.length; i++) {
+      let temp = celsius[i] * 9/5 + 32;
+      truncated = Math.floor(temp)
+      evenArray.push(truncated)
+    }
+    return evenArray;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
